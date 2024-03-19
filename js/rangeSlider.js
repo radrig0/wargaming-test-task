eb6b26666009ac46bba7b94a3ce805b8f511184a
merokhin <erohin.mihail@gmail.com>
2024-03-19 18:49:48 +0100

    base experience counter

--- js/rangeSlider.js
+++ js/rangeSlider.js
@@ -1,10 +1,37 @@
-var slider = document.getElementById("rangeSlider");
-var output = document.getElementById("rangeInput");
+const slider = document.getElementById("rangeSlider");
+const output = document.getElementById("rangeInput");
 output.innerHTML = slider.value; // Display the default slider value
 
+const experienceCounter = document.getElementById("experienceValue")
+const INTERNAL_FACTOR = 3
+
+function calculateExperience() {
+    const configurationEls = document.getElementsByName("configuration")
+    let configurationFactor = 1
+    for (i = 0; i < configurationEls.length; i++) {
+        if (configurationEls[i].checked) {
+            configurationFactor = configurationEls[i].value
+        }
+    }
+    experienceCounter.innerHTML = slider.value * INTERNAL_FACTOR * configurationFactor
+}
+
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
+slider.oninput = function () {
     output.value = this.value;
+    calculateExperience()
+}
+
+output.oninput = function () {
+    slider.value = this.value
+    calculateExperience()
+}
+
+let rad = document.getElementsByName("configuration");
+for (let i = 0; i < rad.length; i++) {
+    rad[i].addEventListener('change', function() {
+        calculateExperience()
+    });
 }
 
 for (let e of document.querySelectorAll('input[type="range"].rangeSlider')) {
