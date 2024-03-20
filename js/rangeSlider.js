2d25cf459a48250c34d59168ebc19b3238bd28b7
merokhin <erohin.mihail@gmail.com>
2024-03-20 10:29:53 +0100

    remove warnings

--- js/rangeSlider.js
+++ js/rangeSlider.js
@@ -36,7 +36,7 @@ for (let i = 0; i < rad.length; i++) {
 
 for (let e of document.querySelectorAll('input[type="range"].rangeSlider')) {
     e.style.setProperty('--value', e.value);
-    e.style.setProperty('--min', e.min == '' ? '0' : e.min);
-    e.style.setProperty('--max', e.max == '' ? '100' : e.max);
+    e.style.setProperty('--min', e.min === '' ? '0' : e.min);
+    e.style.setProperty('--max', e.max === '' ? '100' : e.max);
     e.addEventListener('input', () => e.style.setProperty('--value', e.value));
 }
